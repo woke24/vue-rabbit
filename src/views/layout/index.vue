40a0774ed0ab863f473164c3180f73e56074d879
woke555 <498867012@qq.com>
2023-08-05 15:30:48 +0800

    layout静态

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,11 +1,22 @@
+<script setup>
+import LayoutNav from './LayoutNav.vue'
+import LayoutHeader from './LayoutHeader.vue'
+import LayoutFooter from './LayoutFooter.vue'
+
+
+
+</script>
 <template>
-  <div>我首页</div>
+  <!-- <div>我首页</div> -->
   <!-- 二级路由出口 -->
+  <LayoutNav />
+  <LayoutHeader />
   <RouterView />
-  <div class="test">test scss</div>
+  <LayoutFooter />
+  <!-- <div class="test">test scss</div> -->
 </template>
 <style scoped lang="scss">
-.test {
-  color: $priceColor;
-}
+// .test {
+//   color: $priceColor;
+// }
 </style>
